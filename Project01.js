9a05d3ab86eee2e1b1a67357cbf16d9dc9abbff6
Leia Kamal Sayed <lksay3d@gmail.com>
2024-04-10 02:47:51 -0400

    Concrete Products: 90 & 00

--- Project01.js
+++ Project01.js
@@ -17,3 +17,42 @@ class Textbox{
         throw new Error("Method 'type()' must be implemented.");
     }
 }
+
+//defining concrete products
+//Word90 Concrete Products
+class Word90Panel extends Panel{
+    draw(){
+        console.log("Drawing a Panel for Word90");
+    }
+}
+
+class Word90Button extends Button{
+    click(){
+        console.log("Clicking a Button for Word90");
+    }
+}
+
+class Word90Textbox extends Textbox{
+    type(){
+        console.log("Typing in a Textbox for Word90");
+    }
+}
+
+//Word00 Concrete Products
+class Word00Panel extends Panel{
+    draw(){
+        console.log("Drawing a Panel for Word00");
+    }
+}
+
+class Word00Button extends Button{
+    click(){
+        console.log("Clicking a Button for Word00");
+    }
+}
+
+class Word00Textbox extends Textbox{
+    type(){
+        console.log("Typing in a Textbox for Word00");
+    }
+}
